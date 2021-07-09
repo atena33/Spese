e3fbb439a2258b5d4dbe95d8672ff2524f7e114f
anna.mura <anna.mura@WINAPCXOKL5HOO8>
2021-07-09 09:14:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Spese
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
